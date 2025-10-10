a7b8a61408e0f96ba1e3c7a25ab391a66eaf2f1b
sramorosi <sramorosi@users.noreply.github.com>
2025-10-09 17:12:30 -0700

    Naila new work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ConceptAprilTag_Nf.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ConceptAprilTag_Nf.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -64,7 +64,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "Concept: AprilTag Localization", group = "Concept")
+@TeleOp(name = "AprilTag-Nf", group = "Concept")
 @Disabled
 public class ConceptAprilTag_Nf extends LinearOpMode {
 
