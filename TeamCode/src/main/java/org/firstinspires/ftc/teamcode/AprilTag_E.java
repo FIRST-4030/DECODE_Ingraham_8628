a53295f59e5012640a5f24fbe043ea54f09293c0
Laptop23 <>
2025-10-16 17:33:38 -0700

    Began implementation of automatic side and obelisk detection.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTag_E.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTag_E.java
@@ -39,6 +39,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -95,6 +97,77 @@ public class AprilTag_E {
     private double yaw;
     private double range;
 
+    private boolean blueSide;
+    private boolean redSide;
+    private boolean PPG,PGP,GPP;
+
+    public void scanField(Telemetry telemetry){
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        if(!currentDetections.isEmpty()) {
+            for (AprilTagDetection detection : currentDetections) {
+                if (detection.id == goalTagId) {
+                    ledColor = "green";
+                    if (detection.metadata != null) {
+                        if (detection.id == 20){
+                            blueSide = true;
+                            redSide = false;
+                        }
+                        if (detection.id == 24){
+                            blueSide = false;
+                            redSide = true;
+                        }
+                        if (detection.id == 22){ //PGP
+                            PPG = false;
+                            GPP = false;
+                            PGP = true;
+                        }
+                        if (detection.id == 23){ //PPG
+                            PPG = true;
+                            GPP = false;
+                            PGP = false;
+                        }
+                        if (detection.id == 21){ //GPP
+                            PPG = false;
+                            GPP = true;
+                            PGP = false;
+                        }
+//                        distanceToGoal = detection.ftcPose.y;
+//                        telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+//                        telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+//                        telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+//                        telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+//                        bearing = detection.ftcPose.bearing;
+//                        yaw = detection.ftcPose.yaw;
+                        telemetry.addData("Blue; ",blueSide);
+                        telemetry.addData("Red; ", redSide);
+                        telemetry.addData("PPG; ",PPG);
+                        telemetry.addData("PGP; ",PGP);
+                        telemetry.addData("GPP; ",GPP);
+                        telemetry.update();
+                    }
+
+//                        telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+//                        telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+//                    }
+                }
+            }
+        }
+
+        else{
+            bearing = 999;
+            ledColor = "red";
+        }
+
+        // Add "key" information to telemetry
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+    }
 
     public void runInLoop(Telemetry telemetry) {
 
@@ -120,8 +193,8 @@ public class AprilTag_E {
     /**
      * Initialize the AprilTag processor.
      */
-    public void initAprilTag(int paramTagId, HardwareMap hardwareMap) {
-        goalTagId = paramTagId;
+    public void initAprilTag(HardwareMap hardwareMap) {
+
         // Create the AprilTag processor.
         aprilTag = new AprilTagProcessor.Builder()
 
@@ -131,7 +204,7 @@ public class AprilTag_E {
             //.setDrawTagOutline(true)
             //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
             //.setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
-            //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+            .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
 
             // == CAMERA CALIBRATION ==
             // If you do not manually specify calibration parameters, the SDK will attempt
