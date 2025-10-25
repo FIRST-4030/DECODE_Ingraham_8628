af306ff235b11a558173fab7c1b6030323216550
Laptop23 <>
2025-10-25 10:34:12 -0700

    Added shooter class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -4,15 +4,34 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+
 public class Shooter {
     DcMotorEx shooter;
-    public void init(HardwareMap hardwareMap){
+    double power=0;
+
+    public void init(HardwareMap hardwareMap ){
         shooter = (DcMotorEx) hardwareMap.get(DcMotor.class, "shooter");
         shooter.setDirection(DcMotor.Direction.FORWARD);
         shooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         shooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
+    public void initPower(double power){
+        this.power = power;
+    }
+
+    public void shoot(){
+        shooter.setPower(power);
+    }
+
+    public void stop(){
+        shooter.setPower(0);
+    }
+
+    public double adjustPower(double value){
+        power += value;
+        return power;
+    }
 //shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 }
