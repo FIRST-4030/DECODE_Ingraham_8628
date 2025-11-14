128fb80ee8346ec0cde291f3d22cc8b9bcf89cf5
etluconk <edson@edsonjames.com>
2025-11-13 17:40:46 -0800

    Initial implementation of custom blackboard

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Blackboard_ETJ.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+public class Blackboard_ETJ {
+    public static enum Alliance {
+        RED,
+        BLUE,
+        UNKNOWN,
+    }
+    public static Alliance alliance = Alliance.UNKNOWN;
+
+    public static String getAllianceAsString() {
+        if (alliance == Alliance.RED) {
+            return "Red";
+        } else if (alliance == Alliance.BLUE) {
+            return "Blue";
+        } else if (alliance == Alliance.UNKNOWN) {
+            return "Unknown";
+        }
+        return "Undefined";
+    }
+}
